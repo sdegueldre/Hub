46867a9ec5f51203493ee1e165063c0562cb8db7
sdegueldre <42469486+sdegueldre@users.noreply.github.com>
2018-09-13 15:06:15 +0100

    Add software-renderer to the hub

---
+++ p5js/software-renderer/3D.js
@@ -0,0 +1,54 @@
+function Vertex(x,y,z){
+	this.x = parseFloat(x);
+	this.y = parseFloat(y);
+	this.z = parseFloat(z);
+}
+
+function Face(v1,v2,v3){
+	this.v1 = parseInt(v1);
+	this.v2 = parseInt(v2);
+	this.v3 = parseInt(v3);
+}
+
+function normal(f){
+	u = sub(vertices[f.v1], vertices[f.v2]);
+	v = sub(vertices[f.v2], vertices[f.v3]);
+	return normalize(cross(u,v));
+}
+
+function dot(u, v){
+	return u.x*v.x+u.y*v.y+u.z*v.z;
+}
+
+function sub(a, b){
+	return new Vertex(a.x-b.x, a.y-b.y, a.z-b.z);
+}
+
+function cross(u, v){
+	return new Vertex(u.y*v.z-u.z*v.y, u.z*v.x-u.x*v.z, u.x*v.y-u.y*v.x);
+}
+
+function center(f){
+	return new Vertex(	(vertices[f.v1].x + vertices[f.v2].x + vertices[f.v3].x)/3,
+						(vertices[f.v1].y + vertices[f.v2].y + vertices[f.v3].y)/3,
+						(vertices[f.v1].z + vertices[f.v2].z + vertices[f.v3].z)/3)
+}
+
+function normalize(v){
+	let norm = length(v);
+	return new Vertex(v.x/norm, v.y/norm, v.z/norm);
+}
+
+function length(v){
+	return sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
+}
+
+function mult(m, v){
+	return new Vertex(	m[0][0]*v.x+m[0][1]*v.y+m[0][2]*v.z,
+						m[1][0]*v.x+m[1][1]*v.y+m[1][2]*v.z,
+						m[2][0]*v.x+m[2][1]*v.y+m[2][2]*v.z)
+}
+
+function drawTriangle(face, image){
+	
+}
---
+++ p5js/software-renderer/sketch.js
@@ -0,0 +1,115 @@
+//Use a separate script to load an obj file as a string in the file global var!
+
+var vertices = [];
+var fvertices = [];
+var faces = [];
+var renderNormals = false;
+var normalsLength = 0.2;
+var matrix;
+var rotY = 0;
+var turnRate = 0;
+var lightTurnRate = 0;
+var ldir;
+var doShading = true;
+var zBuffer;
+
+function preload(){
+	parse_file();
+}
+
+function setup(){
+	createCanvas(windowWidth, windowHeight);
+	noStroke();
+	strokeWeight(0.005);
+	noSmooth();
+	angleMode(DEGREES);
+	updateMatrix();
+	ldir = new Vertex(0.577,0.577,0.577);
+	zBuffer = createImage(windowWidth, windowHeight);
+	zBuffer.loadPixels();
+}
+
+function draw(){
+	rotateScene(0,turnRate,0);
+	rotateLight();
+	background(0);
+	rotate(180)
+	translate(-width/2, -height+50);
+	scale(200);
+	render();
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+	zBuffer = createImage(windowWidth, windowHeight);
+	zBuffer.loadPixels();
+}
+
+function parse_file(){
+	//pushing an arbitrary vertex at index 0 since obj faces index vertices from 1
+	fvertices.push(new Vertex(0,0,0));
+
+	file = file.split("\n");
+	for(e of file){
+		e = e.split(" ");
+		if(e[0] == 'v')
+			fvertices.push(new Vertex(e[1], e[2], e[3]));
+		if(e[0] == 'f')
+			faces.push(new Face(e[1], e[2], e[3]));
+	}
+}
+
+function render(){
+	for(f of faces){
+		if(f.normal.z > 0){
+			// gamma correction
+			let col = pow(map(dot(f.normal, ldir),-1,1,0,1), 2.2)*255;
+			fill(col);
+			stroke(col);
+			triangle(vertices[f.v1].x, vertices[f.v1].y, vertices[f.v2].x, vertices[f.v2].y, vertices[f.v3].x, vertices[f.v3].y);
+		}
+	}
+}
+/*
+function drawFace(n){
+	let f = faces[n];
+	triangle(vertices[f.v1].x, vertices[f.v1].y, vertices[f.v2].x, vertices[f.v2].y, vertices[f.v3].x, vertices[f.v3].y);
+}*/
+
+//sf = size factor
+function drawVect(v, origin, sf){
+	line(origin.x, origin.y, origin.x+v.x*sf, origin.y+v.y*sf);
+}
+
+function updateMatrix(){
+	matrix = [	[cos(rotY), 0, -sin(rotY)],
+				[0,1,0],
+				[sin(rotY), 0, cos(rotY)]];
+	for(let i = 0; i < fvertices.length; i++)
+		vertices[i] = mult(matrix, fvertices[i]);
+	for(f of faces){
+		f.normal = normal(f);
+		f.center = center(f);
+	}
+}
+
+function rotateScene(x,y,z){
+	rotY += y;
+	updateMatrix();
+}
+
+function rotateLight(){
+	ldir = new Vertex(ldir.x*cos(lightTurnRate)-ldir.z*sin(lightTurnRate),ldir.y,ldir.x*sin(lightTurnRate)+ldir.z*cos(lightTurnRate))
+}
+
+function keyPressed(){
+	switch(keyCode){
+		case LEFT_ARROW:
+		turnRate -= 0.5;
+		break;
+		case RIGHT_ARROW:
+		turnRate += 0.5;
+		break;
+	}
+
+}
