0772856211534e3092c67260d0adf46bde37762c
Samuel Degueldre <samuel.f.degueldre+github@gmail.com>
2018-09-18 16:27:32 +0100

    Added comments

--- p5js/orthogonal-3D/sketch.js
+++ p5js/orthogonal-3D/sketch.js
@@ -21,7 +21,7 @@ function setup(){
 	strokeWeight(lineWidth);
 	noFill();
 	textSize(20);
-	textStyle()
+	textStyle();
 	textAlign(LEFT, TOP);
 
 	vertices.push(new Point(0,1,1.5))
@@ -122,6 +122,7 @@ function setup(){
 	edges.push(new Pair(7,19))
 	edges.push(new Pair(19,27))
 
+	// Swap y and z and invert z so that the model is not upside down
 	for(v of vertices){
 		t = v.y;
 		v.y = v.z;
@@ -142,12 +143,18 @@ var maxDepth;
 var minDepth;
 
 function draw(){
-	background(0, 255);
+	// Background has to be painted in BLEND mode (black is never lightest)
+	blendMode(BLEND);
+	background(0);
 	blendMode(LIGHTEST);
-	//blendMode(ADD);
 	push();
+
+	// Go to center before drawing the heart
 	translate(width/2, height/2);
+	// Convert all vertices to screen space
 	transform();
+
+	// Compute min and max depth for color grading (farther edges are darker)
 	minDepth = maxDepth = 0;
 	for(e of edges){
 		e.depth = (depth(transformed[e.a])+depth(transformed[e.b]))/2;
@@ -156,6 +163,8 @@ function draw(){
 		if(e.depth < minDepth)
 			minDepth = e.depth;
 	}
+
+	// Pain all edges
 	strokeWeight(lineWidth);
 	for(e of edges){
 		let brightness = map(e.depth, minDepth, maxDepth, 255, 20);
@@ -163,9 +172,10 @@ function draw(){
 		line(rasterized[e.a].x, rasterized[e.a].y, rasterized[e.b].x, rasterized[e.b].y);
 	}
 	pop();
+
+	// rotate scene by speed
 	yaw += speed;
 
-	blendMode(BLEND);
 	fill(255);
 	noStroke();
 	text("FPS: " + fps + "\nLine width: " + lineWidth, 10, 30);
@@ -191,8 +201,10 @@ function Pair(a,b){
 function transform(){
 	transformed = [];
 	rasterized = [];
+	// Transform vertices from object space to world space
 	for(v of vertices)
 		transformed.push(new Point((v.x*cos(yaw)-v.y*sin(yaw))*zoom, (v.x*sin(yaw)+v.y*cos(yaw))*zoom, v.z*zoom));
+	// Project vertices from world space to screen space
 	for(t of transformed)
 		rasterized.push(new Point(t.x, t.z*cos(pitch)+sin(pitch)*t.y, 0));
 }
@@ -224,5 +236,6 @@ function keyPressed(){
 }
 
 function depth(p){
+	// Depth is calculated in camera space (depth(p) could be replaced by p.z and have calculation in transform())
 	return cos(pitch)*p.y-sin(pitch)*p.z;
 }
