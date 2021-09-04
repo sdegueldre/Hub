852aa468134d6bec7552e7b6810f6cebea069446
Samuel Degueldre <sad@odoo.com>
2021-09-04 13:11:13 +0200

    [REF/FIX] fix incorrect assignment and refactor brick-breaker

--- p5js/brick-breaker/sketch.js
+++ p5js/brick-breaker/sketch.js
@@ -1,233 +1,234 @@
-var xBricks = 11;
-var yBricks = 6;
-var brickWidth = 90;
-var brickHeight = brickWidth/3;
-var brickSpacing = brickHeight*0.666;
-var Bricks = [xBricks];
-var ps = []; //particle system
+const xBricks = 11;
+const yBricks = 6;
+const brickWidth = 90;
+const brickHeight = brickWidth / 3;
+const brickSpacing = brickHeight * 0.666;
+const bricks = new Set();
+const particles = new Set();
 let lives;
 let score;
 let combo;
 let gameover;
+let paddle, ball;
 
 function setup() {
-	createCanvas(1280,720);
+	createCanvas(1280, 720);
 	background(0);
 	paddle = new Paddle(150, 15);
 	ball = new Ball(15);
-	for(let i = 0; i < xBricks; i++){
-		Bricks[i] = [yBricks];
-		for(let j = 0; j < yBricks; j++){
-			Bricks[i][j] = new Brick(width/2 + (i-(xBricks-1)/2)*(brickWidth+brickSpacing), brickSpacing + brickHeight/2 + (j*(brickHeight+brickSpacing)), brickWidth, brickHeight);
+	bricks.clear();
+	for (let i = 0; i < xBricks; i++) {
+		for (let j = 0; j < yBricks; j++) {
+			bricks.add(new Brick(width / 2 + (i - (xBricks - 1) / 2) * (brickWidth + brickSpacing), brickSpacing + brickHeight / 2 + (j * (brickHeight + brickSpacing)), brickWidth, brickHeight));
 		}
 	}
-  lives = 1;
-  score = 0;
-  combo = 0;
-  gameover = false;
+	lives = 1;
+	score = 0;
+	combo = 0;
+	gameover = false;
 }
 
 function draw() {
-  if(!gameover){
-    paddle.tick();
-  	ball.tick();
-  }
+	if (!gameover) {
+		paddle.tick();
+		ball.tick();
+	}
 	background(0);
 
-	for(let i = 0; i < xBricks; i++)
-		for(let j = 0; j < yBricks; j++){
-			if(Bricks[i][j] != null)
-				Bricks[i][j].draw();
-		}
-
+	for (const brick of bricks) {
+		brick.draw();
+	}
 	paddle.draw();
 	ball.draw();
-	for(let i = 0; i < ps.length; i++){
-		ps[i].draw();
-		ps[i].tick();
-		if(ps[i].life <= 0)
-			ps.splice(i, 1);
+	for (const particle of particles) {
+		particle.draw();
+		particle.tick();
+		if (particle.life <= 0)
+			particles.delete(particle);
 	}
 
-  drawLives();
-  drawScore();
-  if(gameover){
-    fill(127, 127);
-    rect(0,0,width, height);
-    drawGameOver();
-  }
+	drawLives();
+	drawScore();
+	if (gameover) {
+		fill(127, 127);
+		rect(0, 0, width, height);
+		drawGameOver();
+	}
 }
 
-function drawGameOver(){
-  push();
-  fill(0);
+function drawGameOver() {
+	push();
+	fill(0);
 	stroke(255);
 	strokeWeight(4);
 	textSize(100);
 	textAlign(CENTER, BASELINE);
-	text('GAME OVER', width/2, height/2 - 20);
-  textSize(50);
+	text('GAME OVER', width / 2, height / 2 - 20);
+	textSize(50);
 	textAlign(CENTER, TOP);
-	text('press space to play again', width/2, height/2 + 20);
+	text('press space to play again', width / 2, height / 2 + 20);
 	textAlign(CENTER, TOP);
-	text('Final score: '+score, width/2, 3*height/4);
+	text('Final score: ' + score, width / 2, 3 * height / 4);
 	pop();
 }
 
-function drawLives(){
-  push();
-  fill(0);
+function drawLives() {
+	push();
+	fill(0);
 	stroke(255);
 	strokeWeight(4);
 	textSize(50);
 	textAlign(RIGHT, BASELINE);
-	text(lives, width-3.5, height-7);
+	text(lives, width - 3.5, height - 7);
 	pop();
 }
 
-function drawScore(){
-  push();
-  fill(0);
+function drawScore() {
+	push();
+	fill(0);
 	stroke(255);
 	strokeWeight(4);
 	textSize(50);
 	textAlign(LEFT, BASELINE);
-	text(score, 3.5, height-7);
+	text(score, 3.5, height - 7);
 	pop();
 }
 
-function Paddle(w,h) {
-	this.width = w;
-	this.height = h;
-	this.x = width/2;
-	this.y = height-25;
-	this.speed = 15;
-
-	this.draw = function() {
-		noStroke();
-		fill(255);
-		rect(this.x-this.width/2, this.y-this.height/2, this.width, this.height);
+class Paddle {
+	constructor(w, h) {
+		this.w = w;
+		this.h = h;
+		this.x = width / 2;
+		this.y = height - 25;
+		this.speed = 15;
 	}
 
-	this.tick = function() {
-		if(keyIsDown(RIGHT_ARROW))
+	draw() {
+		noStroke();
+		fill(255);
+		rect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+	}
+
+	tick() {
+		if (keyIsDown(RIGHT_ARROW))
 			this.x += this.speed;
-		if(keyIsDown(LEFT_ARROW))
+		if (keyIsDown(LEFT_ARROW))
 			this.x -= this.speed;
-		this.x = constrain(this.x, this.width/2 + 1, width-this.width/2 - 1);
+		this.x = constrain(this.x, this.w / 2 + 1, width - this.w / 2 - 1);
 	}
 }
 
-function Ball(size) {
-	this.size = size;
-	this.x = paddle.x;
-	this.y = paddle.y - this.size - 5;
-	this.speed = 10;
-	this.angle = -HALF_PI;
-	this.isDocked = true;
+class Ball {
+	constructor(size) {
+		this.size = size;
+		this.x = paddle.x;
+		this.y = paddle.y - this.size - 5;
+		this.speed = 10;
+		this.angle = -HALF_PI;
+		this.isDocked = true;
+	}
 
-	this.draw = function() {
-    let sz = size + 5;
+	draw() {
+		const sz = this.size + 5;
 		noStroke();
 		fill(255);
-    ellipseMode(CORNER);
-		ellipse(this.x-sz/2, this.y-sz/2, sz, sz);
+		ellipseMode(CORNER);
+		ellipse(this.x - sz / 2, this.y - sz / 2, sz, sz);
 	}
 
-	this.collide = function(paddle){
-		if((this.x + this.size/2) >= (paddle.x - paddle.width/2) &&
-       (this.x - this.size/2) <= (paddle.x + paddle.width/2) &&
-       (this.y + this.size/2) >= (paddle.y - paddle.height/2) &&
-       (this.y - this.size/2) <= (paddle.y + paddle.height/2)){
-			return true;
-		}
-		return false;
+	collide(paddle) {
+		const collision = ((this.x + this.size / 2) >= (paddle.x - paddle.w / 2) &&
+			(this.x - this.size / 2) <= (paddle.x + paddle.w / 2) &&
+			(this.y + this.size / 2) >= (paddle.y - paddle.h / 2) &&
+			(this.y - this.size / 2) <= (paddle.y + paddle.h / 2))
+		return collision;
 	}
 
-	this.tick = function() {
-		if(this.isDocked)
+	tick() {
+		if (this.isDocked)
 			this.x = paddle.x;
 		else {
-			this.x += this.speed*cos(this.angle);
-			this.y += this.speed*sin(this.angle);
-			if(this.x <= this.size/2 || this.x > width - this.size/2)
+			this.x += this.speed * cos(this.angle);
+			this.y += this.speed * sin(this.angle);
+			if (this.x <= this.size / 2 || this.x > width - this.size / 2)
 				this.angle = -this.angle + PI;
-			if(this.y <= this.size/2)
+			if (this.y <= this.size / 2)
 				this.angle = -this.angle;
-			if(this.collide(paddle)){
-        this.angle = ((this.x - paddle.x)/paddle.width-1)*HALF_PI;
-        combo = 0;
-      }
+			if (this.collide(paddle)) {
+				this.angle = ((this.x - paddle.x) / paddle.w - 1) * HALF_PI;
+				combo = 0;
+			}
 
-			for(let i = 0; i < xBricks; i++)
-				for(let j = 0; j < yBricks; j++)
-					if(Bricks[i][j] != null && this.collide(Bricks[i][j])){
-						let counter = random(3,15);
-						for(let i = 0; i < counter; i++){
-							let xv = 0.5*random(-this.speed, this.speed);
-							let yv = 0.5*random(-this.speed, this.speed);
-							ps.push(new Particle(this.x, this.y, xv, yv));
-						}
-						this.angle = -this.angle;
-						Bricks[i][j] = null;
-            combo++;
-            score += combo;
+			for (const brick of bricks) {
+				if (this.collide(brick)) {
+					let counter = random(3, 15);
+					for (let i = 0; i < counter; i++) {
+						let xv = 0.5 * random(-this.speed, this.speed);
+						let yv = 0.5 * random(-this.speed, this.speed);
+						particles.add(new Particle(this.x, this.y, xv, yv));
 					}
+					this.angle = -this.angle;
+					bricks.delete(brick);
+					combo++;
+					score += combo;
+				}
+			}
 
-			if(this.y >= height - this.size/2){
-        lives--;
-        if(lives <= 0){
-          gameover = true;
-        }
+			if (this.y >= height - this.size / 2) {
+				lives--;
+				if (lives <= 0) {
+					gameover = true;
+				}
 				this.isDocked = true;
-        if(!gameover){
-          this.angle = -HALF_PI;
-          this.x = paddle.x;
-  				this.y = paddle.y - this.size - 1;
-        }
-        combo = 0;
+				if (!gameover) {
+					this.angle = -HALF_PI;
+					this.x = paddle.x;
+					this.y = paddle.y - this.size - 1;
+				}
+				combo = 0;
 			}
 		}
 	}
 }
 
 function keyPressed() {
-	if(keyCode == 32 && ball.isDocked)
+	if (keyCode == 32 && ball.isDocked)
 		ball.isDocked = false;
-	if(keyCode == ESCAPE || (keyCode == 32 && gameover))
-    setup();
+	if (keyCode == ESCAPE || (keyCode == 32 && gameover))
+		setup();
 }
 
-function Brick(x,y,w,h) {
-	this.x = x;
-	this.y = y;
-	this.width = w;
-	this.height = h;
+class Brick {
+	constructor(x, y, w, h) {
+		Object.assign(this, { x, y, w, h })
+	}
 
-	this.draw = function() {
+	draw() {
 		noStroke();
 		fill(255);
-		rect(this.x-this.width/2, this.y-this.height/2, this.width, this.height);
+		rect(this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
 	}
 }
 
-function Particle(x,y,xv,yv) {
-	({this} = {x: x, xv: xv, y: y, yv: yv});
-	this.gravity = 0.5;
-	this.r = 3;
-	this.life = 50;
+class Particle {
+	constructor(x, y, xv, yv) {
+		Object.assign(this, {
+			x, y, xv, yv,
+			r: 3, life: 50,
+			gravity: 0.5,
+		});
+	}
 
-
-	this.tick = function() {
+	tick() {
 		this.life--;
 		this.x += this.xv;
 		this.y += this.yv;
 		this.yv += this.gravity;
 	}
 
-	this.draw = function(){
+	draw() {
 		ellipseMode(CENTER);
-		fill(255,255,255, this.life/50*255);
-		ellipse(this.x, this.y, this.r*2, this.r*2);
+		fill(255, 255, 255, this.life / 50 * 255);
+		ellipse(this.x, this.y, this.r * 2, this.r * 2);
 	}
 }
